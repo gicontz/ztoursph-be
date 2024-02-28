023d4000d69425bb2b0f35cc80691a7a44d993c3
selelel <janrusselgorembalem4@gmail.com>
2024-02-28 17:13:21 +0800

    [FEAT] Fetch buffer to s3 bucket

--- src/pdf/pdf.controller.ts
+++ src/pdf/pdf.controller.ts
@@ -1,12 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { PdfService } from './pdf.service';
+import { S3Service } from 'src/aws-sdk/s3.object';
+import { S3BucketService } from 'src/middlewares/s3.service';
 
 @Controller('pdf')
 export class PdfController {
-  constructor(private readonly PDFService: PdfService) {}
+  constructor(
+    private readonly PDFService: PdfService,
+    private readonly s3Service: S3Service,
+    private readonly seServiceBucket: S3BucketService,
+  ) {}
   @Post()
   async getTest(@Body() body: { title: string }) {
-    const pdf = await this.PDFService.GeneratePDF(body.title);
-    return pdf;
+    const pdf = await this.PDFService.GeneratePDF(body.title, body.title);
+    await this.s3Service.uploadFiles([pdf]);
+    return this.seServiceBucket.getPDF(pdf.filename);
   }
 }
