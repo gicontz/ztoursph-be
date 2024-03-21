9bc72fe32058920e0e667e7fb8b94add63acfb5b
gimcontz <gimcontillo@Gims-MacBook-Air.local>
2024-03-22 01:43:52 +0800

    Add docker command

---
+++ ztoursph-docker.sh
@@ -0,0 +1,13 @@
+y | docker system prune
+# Remove all images
+docker rmi $(docker images -a -q)
+
+# Remove all containers
+docker rm $(docker ps -a -q)
+
+# Stop all containers and running images
+docker stop $(docker ps -a -q)
+
+# Build and run the app
+docker build -t ztoursph-service .
+docker run -p 3001:3001 ztoursph-app ztoursph-service
