d20cc562dc968f5db2b647f175b749fa0be2083d
gimcontz <gimcontillo@Gims-MacBook-Air.local>
2024-03-22 01:46:44 +0800

    Change docker sh

--- ztoursph-docker.sh
+++ ztoursph-docker.sh
@@ -1,12 +1,14 @@
 y | docker system prune
+
+# Stop all containers and running images
+docker stop $(docker ps -a -q)
+
 # Remove all images
 docker rmi $(docker images -a -q)
 
 # Remove all containers
 docker rm $(docker ps -a -q)
 
-# Stop all containers and running images
-docker stop $(docker ps -a -q)
 
 # Build and run the app
 docker build -t ztoursph-service .
